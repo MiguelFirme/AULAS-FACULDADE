9853005f10e518a71485e2c0b8692517786375cf
MiguelFirme <miguelgfirme015@gmail.com>
2025-05-06 09:01:33 -0300

    Delete AULAS-PROG-ENGENHARIA directory

--- AULAS-PROG-ENGENHARIA/EXEMPLO 03.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input("digite a 1º nota: "))
-n2 = float(input("digite a 2º nota: "))
-n3 = float(input("digite a 3º nota: "))
-soma = (n1+n2+n3)/3
-print("A média é: ",soma)
--- AULAS-PROG-ENGENHARIA/EXEMPLO 04.py
+++
@@ -1,5 +0,0 @@
-a = float (input("Valor da base: "))
-b = float (input("Valor da altura: "))
-
-area = a * b
-print( " A área do retângulo é: ",area)
